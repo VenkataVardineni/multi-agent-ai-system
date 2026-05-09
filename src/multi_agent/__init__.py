d61b3ba427ccb1ad0fd037ed44af80e303c31ace
VenkataVardineni <vvr2211@umd.edu>
2026-05-09 15:04:14 -0400

    feat(package): export loader, hooks, and journal symbols

--- src/multi_agent/__init__.py
+++ src/multi_agent/__init__.py
@@ -2,19 +2,27 @@
 
 from multi_agent.delegation import DelegationRouter
 from multi_agent.memory import SharedMemory
+from multi_agent.memory_journal import MemoryJournal
+from multi_agent.orchestration_hooks import OrchestrationHooks
 from multi_agent.orchestrator import Orchestrator
 from multi_agent.session import AgentSession
 from multi_agent.tools import ToolRegistry, build_default_registry
-from multi_agent.types import AgentRole, Task, WorkflowStep
+from multi_agent.types import AgentRole, ParsedWorkflow, Task, WorkflowStep
+from multi_agent.workflow_loader import load_workflow_file, parse_workflow_payload
 
 __all__ = [
     "AgentRole",
     "Task",
     "WorkflowStep",
+    "ParsedWorkflow",
     "SharedMemory",
+    "MemoryJournal",
     "AgentSession",
     "DelegationRouter",
+    "OrchestrationHooks",
     "Orchestrator",
     "ToolRegistry",
     "build_default_registry",
+    "load_workflow_file",
+    "parse_workflow_payload",
 ]
