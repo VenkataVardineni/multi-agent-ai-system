406e7de6b46ada3577a12f46542b2d741555670e
VenkataVardineni <vvr2211@umd.edu>
2026-05-24 12:50:05 -0500

    feat: export Message type from package init

--- src/multi_agent/__init__.py
+++ src/multi_agent/__init__.py
@@ -7,11 +7,12 @@ from multi_agent.orchestration_hooks import OrchestrationHooks
 from multi_agent.orchestrator import Orchestrator
 from multi_agent.session import AgentSession
 from multi_agent.tools import ToolRegistry, build_default_registry
-from multi_agent.types import AgentRole, ParsedWorkflow, Task, WorkflowStep
+from multi_agent.types import AgentRole, Message, ParsedWorkflow, Task, WorkflowStep
 from multi_agent.workflow_loader import load_workflow_file, parse_workflow_payload
 
 __all__ = [
     "AgentRole",
+    "Message",
     "Task",
     "WorkflowStep",
     "ParsedWorkflow",
